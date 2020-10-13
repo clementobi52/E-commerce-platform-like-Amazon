77b8e7768faee2853fb3d94a28f60f64ba8a23ba
clementobi52 <47439281+clementobi52@users.noreply.github.com>
2020-10-13 15:47:37 +0100

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export default {
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/amazona",
+  JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'AeAnNVd-6FKzR1U8WCymFbbXe57ZJ2YGKqNCHHobbD4F8bI6R_Y56f5aCEKU8kLpIgYyAWJmS2P0dXsB'
+};
